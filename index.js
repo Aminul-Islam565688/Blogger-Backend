7b506517ab5faef3a7a06fa1be084f9d8bd52c99
Aminul islam <aminulislam565688@gmail.com>
2021-06-25 16:39:33 +0600

    i finishied almost everything

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
-
+const { MongoClient, ObjectID, ObjectId } = require('mongodb');
+const fileUpload = require('express-fileupload')
 const app = express()
+
+
 app.use(bodyParser.json());
 app.use(cors());
+app.use(fileUpload())
+
 const port = 4564
 
-console.log('hello everything is fine');
+
+const uri = "mongodb+srv://test:test@cluster0.aifw0.mongodb.net/Blogger?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("Blogger").collection("Admin");
+    const BlogCollection = client.db("Blogger").collection("Blogs");
+    // perform actions on the collection object
+
+    // for Login
+    app.post('/login', (req, res) => {
+        const adminEmail = req.body.email;
+        const adminPassword = req.body.password;
+        collection.find({ email: adminEmail, password: adminPassword })
+            .toArray((err, document) => {
+                res.send(document)
+            })
+    })
+
+    // for Post Blog
+    app.post('/postblog', (req, res) => {
+        const file = req.files.file
+        const title = req.body.title
+        const content = req.body.content
+
+        const newImg = file.data;
+        const encImg = newImg.toString('base64')
+
+        const image = {
+            contentType: file.mimeType,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        }
+
+        BlogCollection.insertOne({ image, title, content })
+            .then((result) => {
+                res.send(result.insertedCount > 0)
+            })
+
+    })
+
+    // for Get all Blogs
+    app.get('/allblogs', (req, res) => {
+        BlogCollection.find({})
+            .toArray((err, document) => {
+                res.send(document)
+            })
+    })
+
+    // for Specific Blogs Data
+    app.get('/blog/:id', (req, res) => {
+        const id = req.params.id;
+        BlogCollection.find({ "_id": ObjectId(id) })
+            .toArray((err, document) => {
+                res.send(document);
+            })
+    })
+
+
+    console.log('MongoDB is Connected');
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
