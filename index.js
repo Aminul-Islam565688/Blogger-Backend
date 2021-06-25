34b13a1771527ea7f9ff4a9cbb1ee68187a0a856
Aminul islam <aminulislam565688@gmail.com>
2021-06-26 01:17:56 +0600

    i got heroku error at deploy time

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(fileUpload())
 
-const port = 4564
+const port = process.env.PORT || 4564
 
 
 const uri = "mongodb+srv://test:test@cluster0.aifw0.mongodb.net/Blogger?retryWrites=true&w=majority";
